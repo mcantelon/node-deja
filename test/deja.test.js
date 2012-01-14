0806e7f17ad44f7857682929cf68ffcf5881bb39
Mike Cantelon <mcantelon@gmail.com>
2012-01-14 15:37:21 -0800

    changed variable name and add note about mocha timeout setting.

--- test/deja.test.js
+++ test/deja.test.js
@@ -4,7 +4,9 @@ var assert = require('assert')
   , fs = require('fs')
   , path = require('path')
 
-var TESTING_DIR = '/tmp/dejaTest'
+console.log('NOTE: Tests should be run using: mocha -t 10000')
+
+var testingDir = '/tmp/dejaTest'
 
 function testingSetup(testingDir, callback) {
 
@@ -136,19 +138,19 @@ function testCloneThenTeardown(testingDir, repoBaseName, repoUrl) {
 
 describe('deja', function() {
   beforeEach(function(done) {
-    testingSetup(TESTING_DIR, done)
+    testingSetup(testingDir, done)
   })
 
   afterEach(function(done) {
-    testing_teardown(TESTING_DIR, done)
+    testing_teardown(testingDir, done)
   })
 
   it('should be able to create a .deja directory', function(done) {
-    var deja = spawnInTestHome('deja', ['help'], TESTING_DIR)
+    var deja = spawnInTestHome('deja', ['help'], testingDir)
     deja.on('exit', function(code) {
       code.should.equal(0)
       if (code == 0) {
-        path.exists(TESTING_DIR + '/.deja', function(exists) {
+        path.exists(testingDir + '/.deja', function(exists) {
           exists.should.equal(true)
           done()
         })
@@ -157,24 +159,24 @@ describe('deja', function() {
   })
 
   it('should be able to clone a git repo', function(done) {
-    testClone(TESTING_DIR, 'dotfiles', 'git://github.com/mcantelon/dotfiles.git', function() {
+    testClone(testingDir, 'dotfiles', 'git://github.com/mcantelon/dotfiles.git', function() {
       done()
     })
   })
 
   it('should be able to clone a github repo using shorthand', function(done) {
-    testClone(TESTING_DIR, 'dotfiles', 'mcantelon/dotfiles', function() {
+    testClone(testingDir, 'dotfiles', 'mcantelon/dotfiles', function() {
       done()
     })
   })
 
   it('should be able to remove repo it has cloned', function(done) {
-    doMockClone(TESTING_DIR, 'dotfiles', function(exists) {
+    doMockClone(testingDir, 'dotfiles', function(exists) {
       exists.should.equal(true)
       if (exists) {
-        var deja = spawnInTestHome('deja', ['rm', 'dotfiles'], TESTING_DIR)
+        var deja = spawnInTestHome('deja', ['rm', 'dotfiles'], testingDir)
         deja.on('exit', function(code) {
-          path.exists(TESTING_DIR + '/.deja/dotfiles', function(exists) {
+          path.exists(testingDir + '/.deja/dotfiles', function(exists) {
             // the repo should have been deleted by "deja rm dotfiles"
             exists.should.equal(false)
             done()
@@ -185,10 +187,10 @@ describe('deja', function() {
   })
 
   it('should be able to list a directory', function(done) {
-    doMockClone(TESTING_DIR, 'dotfiles', function(exists) {
+    doMockClone(testingDir, 'dotfiles', function(exists) {
       exists.should.equal(true)
       if (exists) {
-        var deja = spawnInTestHome('deja', ['ls'], TESTING_DIR)
+        var deja = spawnInTestHome('deja', ['ls'], testingDir)
         deja.stdout.on('data', function(data) {
           data.toString().should.equal("dotfiles\n")
           done()
@@ -198,13 +200,13 @@ describe('deja', function() {
   })
 
   it('should be able to create a symlink', function(done) {
-    doMockClone(TESTING_DIR, 'dotfiles', function(exists) {
+    doMockClone(testingDir, 'dotfiles', function(exists) {
       exists.should.equal(true)
       if (exists) {
-        var deja = spawnInTestHome('deja', ['link', 'dotfiles'], TESTING_DIR)
+        var deja = spawnInTestHome('deja', ['link', 'dotfiles'], testingDir)
         deja.on('exit', function(code) {
           code.should.equal(0)
-          fs.lstat(TESTING_DIR + '/.vimrc', function(err, stats) {
+          fs.lstat(testingDir + '/.vimrc', function(err, stats) {
             stats.isSymbolicLink().should.equal(true)
             done()
           })
@@ -214,20 +216,20 @@ describe('deja', function() {
   })
 
   it('should be able to remove a symlink', function(done) {
-    doMockClone(TESTING_DIR, 'dotfiles', function(exists) {
+    doMockClone(testingDir, 'dotfiles', function(exists) {
       exists.should.equal(true)
       if (exists) {
-        var deja = spawnInTestHome('deja', ['link', 'dotfiles'], TESTING_DIR)
+        var deja = spawnInTestHome('deja', ['link', 'dotfiles'], testingDir)
         deja.on('exit', function(code) {
           code.should.equal(0)
-          fs.lstat(TESTING_DIR + '/.vimrc', function(err, stats) {
+          fs.lstat(testingDir + '/.vimrc', function(err, stats) {
             stats.isSymbolicLink().should.equal(true)
 
-            var deja = spawnInTestHome('deja', ['unlink', 'dotfiles'], TESTING_DIR)
+            var deja = spawnInTestHome('deja', ['unlink', 'dotfiles'], testingDir)
             deja.on('exit', function(code) {
               code.should.equal(0)
 
-              fs.lstat(TESTING_DIR + '/.vimrc', function(err, stats) {
+              fs.lstat(testingDir + '/.vimrc', function(err, stats) {
                 assert.equal(stats, undefined)
                 done()
               })
@@ -241,14 +243,14 @@ describe('deja', function() {
   it(
     'should be able to handle duplicate repo names by padding destination names', function(done) {
     var repoUrl = 'mcantelon/dotfiles'
-    doClone(TESTING_DIR, 'dotfiles', repoUrl, function(exists) {
+    doClone(testingDir, 'dotfiles', repoUrl, function(exists) {
       exists.should.equal(true)
       if (exists) {
         var dejaArgs = ['clone', repoUrl]
-        var deja = spawnInTestHome('deja', dejaArgs, TESTING_DIR)
+        var deja = spawnInTestHome('deja', dejaArgs, testingDir)
         deja.on('exit', function(code) {
           // deja should have padding the repo name as it's a duplicate
-          path.exists(TESTING_DIR + '/.deja/dotfiles_1/.git', function(exists) {
+          path.exists(testingDir + '/.deja/dotfiles_1/.git', function(exists) {
             exists.should.equal(true)
             done()
          })
